22c2766d7a560344171433b9259378bd0dbd8ce8
the.poche <a.pochettino@hotmail.com>
2020-12-31 06:08:47 -0300

    start shop js

--- assets/js/archivo.js
+++ assets/js/archivo.js
@@ -1,14 +1,31 @@
+// scroll del home
+
 $(function() {
-    $('a[href*=#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
   });
+});
 
 
 
-  // intento de carrito
+// intento de carrito
 
-  
+
+document.addEventListener('DOMContentLoaded', () => {
+
+})
+
+ const fetchData = async () => {
+
+    try {
+      const res = await fetch('api.json')
+      const data = await res.json()
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+ }
+
 
    
